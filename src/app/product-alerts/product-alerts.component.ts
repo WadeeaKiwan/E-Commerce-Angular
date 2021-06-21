04e8451070a2a2120c2b1c9c1bed2171b99e2409
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2021-06-21 11:55:19 +0200

    Pass data to a child component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 import { Product } from "../types";
 
 @Component({
@@ -14,6 +14,9 @@ export class ProductAlertsComponent implements OnInit {
   // Pass data from the parent component
   // In cases where the compiler can’t eliminate `null` or `undefined`, we can use the type assertion operator to manually remove them. The syntax is postfix `!`
   @Input() product!: Product;
+
+  // We define a property named `notify` with an @Output() decorator and an instance of EventEmitter() that allows the component to emit an event when the value of the `notify` property changes
+  @Output() notify = new EventEmitter();
   
   constructor() {}
 
